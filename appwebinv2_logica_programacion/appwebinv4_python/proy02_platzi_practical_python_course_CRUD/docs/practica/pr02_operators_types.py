bd02c1d6d00204d0f4adcaa7a61a7707d4207752
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-10 07:16:41 -0500

    proyecto1 - crud python

---
+++ appwebinv2_logica_programacion/appwebinv4_python/proy02_platzi_practical_python_course_CRUD/docs/practica/pr02_operators_types.py
@@ -0,0 +1,30 @@
+print()
+# Concatena
+print("Concatena")
+print("     01: a + b =", 'a' + 'b')
+print("     02: a, b =", 'a', 'b')
+# Divide sin decimal
+print("Divide sin decimal")
+print("     10 // 4 =", 10 // 4)
+# Módulo
+print("Módulo")
+print("     24 % 5 =", 24 % 5)
+# Exponente
+print("Exponente")
+print("     2**8 =", 2**8)
+# Tipos
+print("Tipos")
+print("     Entero  : type(5) =", type(5))
+print("     Decimal : type(2.5) =", type(2.5))
+print("     Cadena  : type('Albeiro') =", type("Albeiro"))
+# Operaciones
+print("Operaciones con cadenas")
+print("     Multiplica la cadena: 'Hola ' * 5 = ", ('Hola ' * 5))
+print("Asignación")
+# Asignación
+a = 5
+print("     a = 5")
+a += 3
+print("     a += 3")
+print("     a =", a)
+print()
---
+++ appwebinv2_logica_programacion/appwebinv4_python/proy02_platzi_practical_python_course_CRUD/main.py
@@ -0,0 +1,72 @@
+clients = 'pablo,ricardo,'
+
+def create_client(client_name):
+    global clients
+
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
+        
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def update_client(client_name, update_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', update_client_name + ',')
+    else:
+        print()
+        print('Client is not in client\'s list')
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
+def _get_client_name():
+    return input('What is the client name?: ')
+
+
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]elete client')
+    print()
+
+
+if __name__ == '__main__':
+    _print_welcome()
+
+    command = input()
+    command = command.upper()
+    print()
+
+    if command == 'C':
+        client_name = _get_client_name()
+        create_client(client_name)
+        print()
+        list_clients()
+    elif command == 'D':
+        pass
+    elif command == 'U':
+        client_name = _get_client_name()        
+        update_client_name = input('What is the update client name?: ')
+        update_client(client_name, update_client_name)
+        print()
+        list_clients()
+    else:
+        print('Invalid command')
+
+print()
