47c4d8c8fa204e7ee62533b75561133792831e1e
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-25 05:42:23 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/pr_practice/pr_02_vars.py
+++ lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/pr_practice/pr_02_vars.py
@@ -1,16 +1,23 @@
-print()
-print("hola, vars")
-# Esto es una variable
-my_name = "Albeiro"
-print(my_name)
-# Esto es una variable con un número
+# Variables
+print('\nVariables')
+my_name = 'Albeiro'
 my_age = 40
-print(my_age)
-# Se cambia el valor de la variable
-my_name = "Santiago"
-print("Aquí cambió", my_name)
-# Solicitar un valor -> input
-my_name = input("Cuál es tu nombre: ")
-my_age = input("Cuál es tu edad: ")
-print("\nHola", my_name, "tienes", my_age, "años")
+print("     my_name = 'Albeiro'")
+print("     my_age = 40")
+print("     print(my_name) =", my_name)
+print("     print(my_age) =", my_age)
+
+# Variables dinámicas
+print('\nVariables dinámicas')
+my_name = 'Santiago'
+print("     my_name = 'Santiago'")
+print("     print(my_name) =", my_name)
+
+# Entrada de datos  : input()
+print('\nEntrada de datos : input()')
+print()
+my_name = input("my_name = input('Cuál es tu nombre: ') : ")
+my_age = input("my_age = input('Cuál es tu edad: ')    : " )
+print()
+print("     print('Hola', my_name, 'tienes', my_age, 'años') = ", 'Hola', my_name, 'tienes', my_age, 'años')
 print()
