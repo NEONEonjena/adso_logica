41daef79cff03a8de0dd1288bbcbfa05b9d0fac3
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-25 12:39:07 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/fd_pr_practice/pr_05_functions.py
+++ lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/fd_pr_practice/pr_05_functions.py
@@ -1,29 +1,61 @@
 # Definir una función
-print("\nDefinir una función")
 print()
-print("     def funcion (parámetros):")
+print("*" * 27, "Definir una función", "*" * 28)
+print()
+print("     def funcion(parámetros):")
 print("         return valor")
 
 # Alcance de las variables: globales y locales
-print("\nAlcance de las variables: globales y locales")
 print()
-# x = 10  # variable global
+print("*" * 15, "Alcance de las variables: globales y locales", "*" * 15)
+
+# No se modifica la variable global
+print("\nNo se modifica la variable global")
+print()
+
+num1 = 5 # variable global
 
 
-# def funcion():
-#     # global x
-#     x = 5  # variable local
-#     return x
+def funcion():    
+    num1 = 50  # variable local
+    return num1
 
 
-# print(funcion())  # imprime 5
-# print(x)  # imprime 5
+print("     num1 = 5 # variable global")
+print()
+print("     def funcion():")
+print("         num1 = 50  # variable local")
+print("         return num1")
+print()
+print("     print(funcion()) =", funcion())
+print("     print(num1) =", num1)
+print()
 
-# print
+# Se modifica la variable global
+print("-" * 76)
+print("Se modifica la variable global")
+print()
 
-print("*" * 28, "Casos en Funciones", "*" * 28)
+
+def funcion():
+    global num1 # Usa la variable global
+    num1 = 50  # Es la variable global
+    return num1
+
+
+print("     num1 = 5 # variable global")
+print()
+print("     def funcion():")
+print("         global num1 # Usa la variable global")
+print("         num1 = 50  # Es la variable global")
+print("         return num1")
+print()
+print("     print(funcion()) =", funcion())
+print("     print(num1) =", num1)
 
 # Caso 01. Función SIN parámetros y SIN retorno de valor
+print()
+print("*" * 28, "Casos en Funciones", "*" * 28)
 print("\nCaso 01. Función SIN parámetros y SIN retorno de valor")
 print()
 
@@ -45,10 +77,11 @@ print("         print('La suma de', num1, 'más', num2, 'es', res)")
 print()
 print("     funcion_caso_01()")
 funcion_caso_01()
+print()
+print("-" * 76)
 
 # Caso 02. Función CON parámetros y SIN retorno de valor
-print("-" * 76)
-print("\nCaso 02. Función CON parámetros y SIN retorno de valor")
+print("Caso 02. Función CON parámetros y SIN retorno de valor")
 print()
 
 
@@ -63,10 +96,11 @@ print("         print('La suma de', num1, 'más', num2, 'es', res)")
 print()
 print("     funcion_caso_02(5, 10)")
 funcion_caso_02(5, 10)
+print()
+print("-" * 76)
 
 # Caso 03. Función SIN parámetros y CON retorno de valor
-print("-" * 76)
-print("\nCaso 03. Función SIN parámetros y CON retorno de valor")
+print("Caso 03. Función SIN parámetros y CON retorno de valor")
 print()
 
 num1 = 5
@@ -87,10 +121,11 @@ print("         return res")
 print()
 print("     print('La suma de', num1, 'más', num2, 'es', funcion_caso_03())")
 print('     = La suma de', num1, 'más', num2, 'es', funcion_caso_03())
+print()
+print("-" * 76)
 
 # Caso 04. Función CON parámetros y CON retorno de valor
-print("-" * 76)
-print("\nCaso 04. Función CON parámetros y CON retorno de valor")
+print("Caso 04. Función CON parámetros y CON retorno de valor")
 print()
 
 
@@ -105,5 +140,6 @@ print("         return res")
 print()
 print("     print('La suma de', num1, 'más', num2, 'es', funcion_caso_04(5, 10))")
 print('     = La suma de', num1, 'más', num2, 'es', funcion_caso_04(5, 10))
+print()
 print("-" * 76)
 print()
