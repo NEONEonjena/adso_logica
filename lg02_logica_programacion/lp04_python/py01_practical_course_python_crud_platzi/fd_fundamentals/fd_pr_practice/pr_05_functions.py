6cc9c3e59cccb69a605aebf95407a83be15bf3ca
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-25 11:54:18 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/fd_pr_practice/pr_05_functions.py
+++ lg02_logica_programacion/lp04_python/py01_practical_course_python_crud_platzi/fd_fundamentals/fd_pr_practice/pr_05_functions.py
@@ -1,17 +1,109 @@
 # Definir una función
 print("\nDefinir una función")
-def suma_de_dos_numeros(x, y):
-    return x + y
-print("     def suma_de_dos_numeros(x,y):")
-print("         return x + y")
 print()
-print("     print(suma_de_dos_numeros(10, 15)) =", suma_de_dos_numeros(10, 15))
-print("     print(type(suma_de_dos_numeros)) =", type(suma_de_dos_numeros))
+print("     def funcion (parámetros):")
+print("         return valor")
 
-# Asignar valor de función a una variable
-print("\nAsignar valor de una función a una variable")
-suma_total = suma_de_dos_numeros(10, 15)
-print("     suma_total = suma_de_dos_numeros(10, 15)")
-print("     print(suma_total) =", suma_total)
-print("     print(type(suma_total)) =", type(suma_total))
+# Alcance de las variables: globales y locales
+print("\nAlcance de las variables: globales y locales")
+print()
+# x = 10  # variable global
+
+
+# def funcion():
+#     # global x
+#     x = 5  # variable local
+#     return x
+
+
+# print(funcion())  # imprime 5
+# print(x)  # imprime 5
+
+# print
+
+print("*" * 28, "Casos en Funciones", "*" * 28)
+
+# Caso 01. Función SIN parámetros y SIN retorno de valor
+print("\nCaso 01. Función SIN parámetros y SIN retorno de valor")
+print()
+
+num1 = 5
+num2 = 10
+
+
+def funcion_caso_01():
+    res = num1 + num2    
+    print('     = La suma de', num1, 'más', num2, 'es', res)
+
+
+print("     num1 = 5")
+print("     num2 = 10")
+print()
+print("     def funcion_caso_01():")
+print("         res = num1 + num2")
+print("         print('La suma de', num1, 'más', num2, 'es', res)")
+print()
+print("     funcion_caso_01()")
+funcion_caso_01()
+
+# Caso 02. Función CON parámetros y SIN retorno de valor
+print("-" * 76)
+print("\nCaso 02. Función CON parámetros y SIN retorno de valor")
+print()
+
+
+def funcion_caso_02(num1, num2):
+    res = num1 + num2
+    print('     = La suma de', num1, 'más', num2, 'es', res)
+
+
+print("     def funcion_caso_02(num1, num2):")
+print("         res = num1 + num2")
+print("         print('La suma de', num1, 'más', num2, 'es', res)")
+print()
+print("     funcion_caso_02(5, 10)")
+funcion_caso_02(5, 10)
+
+# Caso 03. Función SIN parámetros y CON retorno de valor
+print("-" * 76)
+print("\nCaso 03. Función SIN parámetros y CON retorno de valor")
+print()
+
+num1 = 5
+num2 = 10
+
+
+def funcion_caso_03():    
+    res = num1 + num2
+    return res
+
+
+print("     num1 = 5")
+print("     num2 = 10")
+print()
+print("     def funcion_caso_03():")
+print("         res = num1 + num2")
+print("         return res")
+print()
+print("     print('La suma de', num1, 'más', num2, 'es', funcion_caso_03())")
+print('     = La suma de', num1, 'más', num2, 'es', funcion_caso_03())
+
+# Caso 04. Función CON parámetros y CON retorno de valor
+print("-" * 76)
+print("\nCaso 04. Función CON parámetros y CON retorno de valor")
+print()
+
+
+def funcion_caso_04(num1,num2):
+    res = num1 + num2
+    return res
+
+
+print("     def funcion_caso_04(num1, num2):")
+print("         res = num1 + num2")
+print("         return res")
+print()
+print("     print('La suma de', num1, 'más', num2, 'es', funcion_caso_04(5, 10))")
+print('     = La suma de', num1, 'más', num2, 'es', funcion_caso_04(5, 10))
+print("-" * 76)
 print()
