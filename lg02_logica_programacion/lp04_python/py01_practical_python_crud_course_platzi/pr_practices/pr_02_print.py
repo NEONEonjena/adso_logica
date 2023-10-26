f1c1e55ef4d4a8a45f3bd6d8e120d22eb10fb8d7
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-26 15:13:54 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_02_print.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_02_print.py
@@ -2,9 +2,12 @@
 print('-' * 90)
 print('Imprimir en pantalla')
 print()
-print("     print('Imprimir en pantalla') = Imprimir en pantalla")
-print("     print('Hola soy Albeiro y tengo 40 años') = Hola soy Albeiro y tengo 40 años")
-print()
+print("     print('Nombre   : Edwin')       =       Nombre   : Edwin")
+print("     print('Apellido : Ramos')       =       Apellido : Ramos")
+print("     print('Edad     : 40')          =       Edad     : 40")
+print("     print('Estatura : 1.72')        =       Estatura : 1.72")
+print("     print('Grp sang : O')           =       Grp sang : O")
+print("     print('Tip sang : +')           =       Tip sang : +")
 
 # Comentarios
 """
---
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_03_var_const.py
@@ -0,0 +1,62 @@
+# Variables
+print('-' * 80)
+print('Variables')
+print()
+user_name = 'Edwin'
+user_lastname = 'Ramos'
+user_age = 40
+user_height = 1.72
+blood_group = 'O'
+blood_type = '+'
+print("     user_name = 'Edwin'")
+print("     user_lastname = 'Ramos'")
+print("     user_age = 40")
+print("     user_height = 1.72")
+print("     blood_group = 'O'")
+print("     blood_type = '+'")
+print()
+print("     print(user_name) =", user_name)
+print("     print(user_lastname) =", user_lastname)
+print("     print(user_age) =", user_age)
+print("     print(user_height) =", user_height)
+print("     print(blood_group) =", blood_group)
+print("     print(blood_type) =", blood_type)
+print()
+
+# Variables dinámicas
+print('-' * 80)
+print('Variables dinámicas')
+print()
+user_name = 'Albeiro'
+print("     user_name = 'Albeiro'")
+print("     print(user_name) =", user_name)
+print()
+
+# Variables privadas : Guión bajo ( _ )
+print('-' * 80)
+print('Variables privadas : Guión bajo ( _ )')
+print()
+_user_code = 'user_01'
+print("     _user_code = 'user_01'")
+print("     print(_user_code) =", _user_code)
+print()
+
+# Variables super privadas : Doble guión bajo ( __ )
+print('-' * 80)
+print('Variables super privadas : Doble guión bajo ( __ )')
+print()
+__do_not_touch = 'something important'
+print("     __do_not_touch = 'something important'")
+print("     print(__do_not_touch) =", __do_not_touch)
+print()
+
+# Constantes (En mayúscula sostenida)
+print('-' * 80)
+print('Constantes (En mayúscula sostenida)')
+print()
+PI = 3.1416
+print("     PI = 3.1416")
+print("     print(PI) =", PI)
+print()
+print('-' * 80)
+print()
