b0d73cb8102a835a9dd253021003e7590f94c45a
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-27 13:18:04 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
@@ -1,5 +1,5 @@
 # Variables
-print('-' * 200)
+print('-' * 230)
 print("Variables")
 print()
 _user_code = 'user_01'
@@ -21,38 +21,47 @@ print("     user_is_activate = True")
 print()
 
 # Concatenar    : print( , )
-print('-' * 200)
+print('-' * 230)
 print("Concatenar    : print( , )")
 print()
-print("print('Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años ,', 'mi altura es de', + user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)")
-print('\nMi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años ,', 'mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)
+print("print('Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)")
+print('\n     Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)
+print()
+
+# Concatenar    : print( + )
+print('-' * 230)
+print("Concatenar    : print( + )")
+print()
+print("print('Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate)")
+print('\n     Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate))
 print()
 
 # Concatenar    : +
-print('-' * 200)
+print('-' * 230)
 print("Concatenar    : +")
 print()
-template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años,'
-print("     template = _user_code + '. ' + 'Hola, mi nombre es ' + user_name + ' y mi apellido es ' + user_lastname")
+template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(
+    user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp
+print("     template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp")
 print("     print(template) =", template)
 print()
 
-# Concatenar strings : f {}
-print('-' * 200)
+# Concatenar    : f {}
+print('-' * 230)
 print("Concatenar strings : f {}")
 print()
-template = f'{_user_code}. Hola, mi nombre es {user_name} y mi apellido es {user_lastname}'
-print("     template = f'{_user_code}. Hola, mi nombre es {name} y mi apellido es {user_lastname}'")
+template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'
+print("     template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'")
 print("     print(template) =", template)
 print()
 
-# Concatenar strings    : format()
-print('-' * 200)
+# Concatenar    : format()
+print('-' * 230)
 print("Concatenar strings : format()")
 print()
-template = '{}. Hola, mi nombre es {} y mi apellido es {}'.format(_user_code, user_name, user_lastname)
-print("     template = '{}. Hola, mi nombre es {} y mi apellido es {}'.format(_user_code, user_name, user_lastname)")
+template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)
+print("     template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)")
 print("     print(template) =", template)
 print()
-print('-' * 200)
+print('-' * 230)
 print()
