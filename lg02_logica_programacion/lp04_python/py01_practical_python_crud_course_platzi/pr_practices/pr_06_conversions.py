efb007f3d10aadf2efa4f816d156111e723231b3
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-27 12:28:56 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_06_conversions.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_06_conversions.py
@@ -56,52 +56,59 @@ print("     to_string = str(user_height)")
 print("     print(to_string) =", to_string)
 print("     print(type(to_string))", type(to_string))
 print()
-
-# De entero a flotante: float()
-print('-' * 50)
-print("De entero a flotante: float()")
-print()
-variable = 1.72
-variable2 = int(variable)
-print("     variable = 1.72")
-print("     variable2 = int(variable)")
-print("     print(variable) =", variable2)
-print("     print(type(variable)) =", type(variable2))
+to_string = str(user_is_activate)
+print("     to_string = str(user_is_activate)")
+print("     print(to_string) =", to_string)
+print("     print(type(to_string))", type(to_string))
 print()
 
 # De flotante a entero: int()
 print('-' * 50)
 print("De flotante a entero: int()")
 print()
-variable = 1.72
-variable2 = int(variable)
-print("     variable = 1.72")
-print("     variable2 = int(variable)")
-print("     print(variable) =", variable2)
-print("     type(variable) =", type(variable2))
+user_height = 1.72
+to_int = int(user_height)
+print("     user_height = 1.72")
+print("     to_int = int(user_height)")
+print("     print(to_int) =", to_int)
+print("     type(to_int) =", type(to_int))
 print()
 
-
-
-# De flotante a entero: int(4.3) = 4
+# De entero a flotante: float()
 print('-' * 50)
-print("De flotante a entero: int(4.3) = 4")
+print("De entero a flotante: float()")
+print()
+user_age = 40
+to_float = float(user_age)
+print("     user_age = 40")
+print("     to_float = float(user_age)")
+print("     print(to_float) =", to_float)
+print("     print(type(to_float)) =", type(to_float))
 print()
 
-
+# De cadena a entero: int()
+print('-' * 50)
+print("De cadena a entero: int()")
 print()
+user_age = '40'
+print("     user_age = 40")
+print("     print(type(user_age)) =", type(user_age))
+to_int = int(user_age)
+print("     to_int = int(user_age)")
+print("     print(type(to_int)) =", type(to_int))
+print()
+
+# De cadena a flotante: float()
+print('-' * 50)
+print("De cadena a flotante: float()")
+print()
+user_height = '1.72'
+print("     user_height = '1.72'")
+print("     print(type(user_height)) =", type(user_height))
+to_float = float(user_height)
+print("     to_float = float(user_height)")
+print("     print(type(to_float)) =", type(to_float))
+print()
+
 print('-' * 50)
 print()
-
-# print("Mi edad es " + str(age))
-# print(f"Mi edad es {age}")
-# age = input("¿Cuál es tu edad?: ")
-# print(type(age))
-# age = int(age)
-# age += 10
-# print(f"Tu edad en 10 años será {age}")
-# age = int(input("¿Cuál es tu edad?: "))
-# print(type(age))
-# age += 10
-# print(f"Tu edad en 10 años será {age}")
-
