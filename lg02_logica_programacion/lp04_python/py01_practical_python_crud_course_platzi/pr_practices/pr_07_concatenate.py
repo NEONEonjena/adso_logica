6e206e9449f047b1cdd6a5eb217278fa92237a6f
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-28 05:39:55 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
@@ -1,10 +1,9 @@
 # Variables
-print('-' * 230)
-print("Variables")
-print()
+print('\n' + '-' * 230)
+print("Variables\n")
 _user_code = 'user_01'
 user_name = 'Albeiro'
-user_lastname = 'Ramos'
+user_lastname = 'Ramos V.'
 user_age = 40
 user_height = 1.72
 user_blood_gr = 'O'
@@ -18,50 +17,48 @@ print("     user_height = 1.72")
 print("     user_blood_gr = 'O'")
 print("     user_blood_tp = '+'")
 print("     user_is_activate = True")
-print()
 
 # Concatenar    : print( , )
-print('-' * 230)
-print("Concatenar    : print( , )")
-print()
+print('\n' + '-' * 230)
+print("Concatenar    : print( , )\n")
 print("print('Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)")
 print('\n     Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)
-print()
 
 # Concatenar    : print( + )
-print('-' * 230)
-print("Concatenar    : print( + )")
-print()
+print('\n' + '-' * 230)
+print("Concatenar    : print( + )\n")
 print("print('Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate)")
 print('\n     Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate))
-print()
 
 # Concatenar    : +
-print('-' * 230)
-print("Concatenar    : +")
-print()
+print('\n' + '-' * 230)
+print("Concatenar    : +\n")
 template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(
     user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp
 print("     template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp")
 print("     print(template) =", template)
-print()
 
 # Concatenar    : f {}
-print('-' * 230)
-print("Concatenar strings : f {}")
-print()
+print('\n' + '-' * 230)
+print("Concatenar strings : f {}\n")
 template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'
 print("     template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'")
 print("     print(template) =", template)
-print()
 
 # Concatenar    : format()
-print('-' * 230)
-print("Concatenar strings : format()")
-print()
+print('\n' + '-' * 230)
+print("Concatenar strings : format()\n")
 template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)
 print("     template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)")
 print("     print(template) =", template)
-print()
-print('-' * 230)
-print()
+
+# Crear una tabla de datos
+print('\n' + '-' * 230)
+print("Crear una tabla de datos\n")
+print('+' + ('-' * 79) + '+')
+print('| Code\t\t| Nombre\t| Apellido\t| Edad\t| Estat\t| Sang\t| Est\t|')
+print('+' + ('-' * 79) + '+')
+print('| {}\t| {}\t| {}\t| {}\t| {}\t| {} {}\t| {}\t|'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp, user_is_activate))
+print('+' + ('-' * 79) + '+\n')
+
+print(('-' * 230) + '\n')
--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_09_operators_arith.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_09_operators_arith.py
@@ -26,3 +26,29 @@ print("     print(12+5) =", 12+5)
 print("     print(10-5) =", 10-5)
 print("     print(2*3) =", 2*3)
 print("     print(8/2) =", 8/2)
+
+
+# Concatena
+print("\nConcatena")
+print("     01: a + b =", 'a' + 'b')
+print("     02: a, b =", 'a', 'b')
+# Divide sin decimal
+print("Divide sin decimal")
+print("     10 // 4 =", 10 // 4)
+# Módulo
+print("Módulo")
+print("     24 % 5 =", 24 % 5)
+# Exponente
+print("Exponente")
+print("     2**8 =", 2**8)
+# Operaciones
+print("Operaciones con cadenas")
+print("     Multiplica la cadena: 'Hola ' * 5 = ", ('Hola ' * 5))
+print("Asignación")
+# Asignación
+a = 5
+print("     a = 5")
+a += 3
+print("     a += 3")
+print("     a =", a)
+print()
