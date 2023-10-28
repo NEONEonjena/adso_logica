be57c47fec05e8be3979b73eb20fb6031815ba9a
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-28 08:06:02 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_10_while.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_10_while.py
@@ -18,3 +18,30 @@ while (counter < 20):
     if (counter < 15):
         continue
     print(counter)
+
+
+print()
+# While Loops
+print("While Loops")
+print()
+print("     i = 1")
+print("     while i <= 10:")
+print("         print(i)")
+print("         i += 1")
+print()
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+print()
+print("     i = 10")
+print("     while i > 0:")
+print("         print(i)")
+print("         i -= 1")
+print()
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
+
+print()
--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_11_for.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_11_for.py
@@ -37,3 +37,20 @@ for person in people:
     print(person)
 for person in people:
     print('name =>', person['name'])
+
+    print()
+# For Loops
+print("Foor Loops")
+print()
+# range()
+print("Rango: range()")
+print()
+print("     print(range(10))=", range(10))
+print()
+print("     for i in range(10):")
+print("         print(i)")
+print()
+for i in range(10):
+    print(i)
+
+print()
