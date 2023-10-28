a6f9ec3a3b4e8a7045c995efe2c42c9e02030637
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-28 07:48:14 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_07_concatenate.py
@@ -1,5 +1,5 @@
 # Variables
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Variables\n")
 _user_code = 'user_01'
 user_name = 'Albeiro'
@@ -19,19 +19,19 @@ print("     user_blood_tp = '+'")
 print("     user_is_activate = True")
 
 # Concatenar    : print( , )
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Concatenar    : print( , )\n")
 print("print('Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)")
 print('\n     Mi código de usuario es', _user_code,'. Soy', user_name, user_lastname, ', tengo', user_age, 'años , mi altura es de', user_height, 'mts , mi tipo de sangre es', user_blood_gr, user_blood_tp, ', aparezco activo en el sistema, es decir, como', user_is_activate)
 
 # Concatenar    : print( + )
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Concatenar    : print( + )\n")
 print("print('Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate)")
 print('\n     Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp + ', aparezco activo en el sistema, es decir, como ' + str(user_is_activate))
 
 # Concatenar    : +
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Concatenar    : +\n")
 template = 'Mi código de usuario es ' + _user_code + '. Soy ' + user_name + ' ' + user_lastname + ', tengo ' + str(user_age) + ' años, mi altura es de ' + str(
     user_height) + ' mts, mi tipo de sangre es ' + user_blood_gr + user_blood_tp
@@ -39,21 +39,21 @@ print("     template = 'Mi código de usuario es ' + _user_code + '. Soy ' + use
 print("     print(template) =", template)
 
 # Concatenar    : f {}
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Concatenar strings : f {}\n")
 template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'
 print("     template = f'Mi código de usuario es {_user_code}. Soy {user_name} {user_lastname}, tengo {user_age} años, mi altura es de {user_height} mts, mi tipo de sangre es {user_blood_gr}{user_blood_tp}'")
 print("     print(template) =", template)
 
 # Concatenar    : format()
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Concatenar strings : format()\n")
 template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)
 print("     template = 'Mi código de usuario es {}. Soy {} {}, tengo {} años, mi altura es de {} mts, mi tipo de sangre es {}{}'.format(_user_code, user_name, user_lastname, user_age, user_height, user_blood_gr, user_blood_tp)")
 print("     print(template) =", template)
 
 # Crear una tabla de datos
-print('\n' + '-' * 230)
+print('\n' + ('-' * 230))
 print("Crear una tabla de datos\n")
 print('+' + ('-' * 79) + '+')
 print('| Code\t\t| Nombre\t| Apellido\t| Edad\t| Estat\t| Sang\t| Est\t|')
---
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_08_1_operators_assignment.py
@@ -0,0 +1,114 @@
+# ( = ) : Asigna valor a una variable
+print('-' * 60)
+print("( = )  : Asigna valor a una variable\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+
+# ( += ) :     num1 = num1 + num2   Suma
+print('\n' + ('-' * 60))
+print("( += ) :     num1 = num1 + num2      Suma\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 += num2
+print("     num1 += num2")
+print()
+print("     print(num1) =", num1)
+
+# ( -= ) :     num1 = num1 - num2   Resta
+print('\n' + ('-' * 60))
+print("( -= ) :     num1 = num1 - num2      Resta\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 -= num2
+print("     num1 -= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( *= ) :     num1 = num1 * num2   Multiplicación
+print('\n' + ('-' * 60))
+print("( *= ) :     num1 = num1 * num2      Multiplicación\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 *= num2
+print("     num1 *= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( **= ) :     num1 = num1 ** num2 Exponente
+print('\n' + ('-' * 60))
+print("( **= ) :     num1 = num1 ** num2    Exponente\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 **= num2
+print("     num1 **= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( /= ) :     num1 = num1 / num2   División
+print('\n' + ('-' * 60))
+print("( /= ) :     num1 = num1 / num2      División\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 /= num2
+print("     num1 /= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( //= ) :     num1 = num1 // num2 División (Entero)
+print('\n' + ('-' * 60))
+print("( //= ) :     num1 = num1 // num2    División (Entero)\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 //= num2
+print("     num1 //= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( %= ) :     num1 = num1 % num2   Módulo Div
+print('\n' + ('-' * 60))
+print("( %= ) :     num1 = num1 % num2      Módulo Div\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 %= num2
+print("     num1 %= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( &= ) :     num1 = num1 &= num2   Comparación binaria
+print('\n' + ('-' * 60))
+print("( &= ) :     num1 = num1 & num2      Comparación binaria\n")
+num1 = 5
+num2 = 4
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 &= num2
+print("     num1 &= num2")
+print()
+print("     print(num1) =", num1)
+
+
+print('\n' + ('-' * 60) + '\n')
