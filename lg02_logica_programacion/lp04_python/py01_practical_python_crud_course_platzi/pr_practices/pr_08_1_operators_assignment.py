87078ff1e88e3d8805fd567028fcfceaa4c90bff
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-10-30 09:37:01 -0500

    creando instructivo python

--- lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_08_1_operators_assignment.py
+++ lg02_logica_programacion/lp04_python/py01_practical_python_crud_course_platzi/pr_practices/pr_08_1_operators_assignment.py
@@ -1,5 +1,5 @@
 # ( = ) : Asigna valor a una variable
-print('-' * 60)
+print('-' * 72)
 print("( = )  : Asigna valor a una variable\n")
 num1 = 5
 num2 = 3
@@ -7,7 +7,7 @@ print("     num1 = 5")
 print("     num2 = 3")
 
 # ( += ) :     num1 = num1 + num2   Suma
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( += ) :     num1 = num1 + num2      Suma\n")
 num1 = 5
 num2 = 3
@@ -20,7 +20,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( -= ) :     num1 = num1 - num2   Resta
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( -= ) :     num1 = num1 - num2      Resta\n")
 num1 = 5
 num2 = 3
@@ -33,7 +33,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( *= ) :     num1 = num1 * num2   Multiplicación
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( *= ) :     num1 = num1 * num2      Multiplicación\n")
 num1 = 5
 num2 = 3
@@ -46,7 +46,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( **= ) :     num1 = num1 ** num2 Exponente
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( **= ) :     num1 = num1 ** num2    Exponente\n")
 num1 = 5
 num2 = 3
@@ -59,7 +59,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( /= ) :     num1 = num1 / num2   División
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( /= ) :     num1 = num1 / num2      División\n")
 num1 = 5
 num2 = 3
@@ -72,7 +72,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( //= ) :     num1 = num1 // num2 División (Entero)
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( //= ) :     num1 = num1 // num2    División (Entero)\n")
 num1 = 5
 num2 = 3
@@ -85,7 +85,7 @@ print()
 print("     print(num1) =", num1)
 
 # ( %= ) :     num1 = num1 % num2   Módulo Div
-print('\n' + ('-' * 60))
+print('\n' + ('-' * 72))
 print("( %= ) :     num1 = num1 % num2      Módulo Div\n")
 num1 = 5
 num2 = 3
@@ -97,11 +97,11 @@ print("     num1 %= num2")
 print()
 print("     print(num1) =", num1)
 
-# ( &= ) :     num1 = num1 &= num2   Comparación binaria
-print('\n' + ('-' * 60))
-print("( &= ) :     num1 = num1 & num2      Comparación binaria\n")
+# ( &= ) :     num1 = num1 & num2   Comparación binaria 'AND'
+print('\n' + ('-' * 72))
+print("( &= ) :     num1 = num1 & num2      Comparación binaria 'AND'\n")
 num1 = 5
-num2 = 4
+num2 = 3
 print("     num1 = 5")
 print("     num2 = 3")
 print()
@@ -110,5 +110,55 @@ print("     num1 &= num2")
 print()
 print("     print(num1) =", num1)
 
+# ( |= ) :     num1 = num1 | num2   Comparación binaria 'OR'
+print('\n' + ('-' * 72))
+print("( |= ) :     num1 = num1 | num2      Comparación binaria 'OR'\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 |= num2
+print("     num1 |= num2")
+print()
+print("     print(num1) =", num1)
 
-print('\n' + ('-' * 60) + '\n')
+# ( ^= ) :     num1 = num1 ^ num2   Comparación binaria 'XOR'
+print('\n' + ('-' * 72))
+print("( ^= ) :     num1 = num1 ^ num2      Comparación binaria 'XOR'\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 ^= num2
+print("     num1 ^= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( >>= ) :     num1 = num1 >> num2   Desplazamiento binario derecha
+print('\n' + ('-' * 72))
+print("( >>= ) :     num1 = num1 >> num2      Desplazamiento binario derecha\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 >>= num2
+print("     num1 >>= num2")
+print()
+print("     print(num1) =", num1)
+
+# ( <<= ) :     num1 = num1 << num2   Desplazamiento binario izquierda
+print('\n' + ('-' * 72))
+print("( <<= ) :     num1 = num1 << num2      Desplazamiento binario izquierda\n")
+num1 = 5
+num2 = 3
+print("     num1 = 5")
+print("     num2 = 3")
+print()
+num1 <<= num2
+print("     num1 <<= num2")
+print()
+print("     print(num1) =", num1)
+print('\n' + ('-' * 72) + '\n')
